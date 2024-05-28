53f51fa91cc408a18c2e296c6c28c7b1b28a5370
Juan Alberto Cuevas Juarez <136052531+juan-albertoC@users.noreply.github.com>
2024-05-27 23:38:23 -0600

    new paquete main y nueva clase Main para hacer pruebas con instancias, objetos invocar sus atributos y metodos

---
+++ src/com/aluracursos/app2_radioAlura/main/Main.java
@@ -0,0 +1,40 @@
+/*
+Juan Alberto Cuevas Juarez
+27/05/2024
+POO
+ */
+
+package com.aluracursos.app2_radioAlura.main;   //paquete main
+
+import com.aluracursos.app2_radioAlura.modelos.Cancion;  //importar paquete modelos y nombre de la subclase Cancion
+import com.aluracursos.app2_radioAlura.modelos.Podcast;  //importar paquete modelos y nombre de la subclase Podcast
+
+public class Main {     //clase Main dentro del paquete main
+
+    public static void main(String[] args) {   //metodo main para ejecutar test
+
+        Cancion miCancion = new Cancion();  //crear instancia 1 cuyo objeto ya almacenado en memoria
+        miCancion.setTitulo("Forever");     //invocar objeto con sus atributos
+        miCancion.setCantante("Kiss");      //invocar objeto con sus atributos
+
+        Podcast miPodcast = new Podcast();        //crear instancia 2 cuyo objeto ya almacenado en memoria
+        miPodcast.setPresentador("Juan Cuevas");  //invocar objeto con sus atributos
+        miPodcast.setTitulo("Tech Job");          //invocar objeto con sus atributos
+
+        //test de metodo meGusta() para objeto miCancion
+        for (int i = 0; i < 100; i++) {
+            miCancion.meGusta();
+        }
+
+        //test de metodo reproducir() para objeto miCancion
+        for (int i = 0; i < 5000; i++) {
+            miCancion.reproducir();
+        }
+
+        //Imprimir valores
+        System.out.println("--------------Imprimir valores-----------");
+        System.out.println("Total de reproducciones: " + miCancion.getTotalDeReproducciones());
+        System.out.println("Total de me Gusta: " + miCancion.getTotalDeMeGustas());
+
+    }
+}
